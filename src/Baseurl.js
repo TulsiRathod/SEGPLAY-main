d8acb5d7a39aa0599c2c473fce1e2e5b55b17107
parth1672 <mistriparth786@gmail.com>
2023-01-29 15:59:13 +0530

    chnage ip

--- src/Baseurl.js
+++ src/Baseurl.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const SERVER_URL = "http://20.219.164.153:3000/";
+export const SERVER_URL = "http://20.219.164.153/";
 export const ROUND_DELAY = 60;
 
 export const socket = io(SERVER_URL);
